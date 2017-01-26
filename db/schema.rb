a0b1434d98d551f0e69134cf5638d5e0627e808d
Patrick Kolodgy <pkolodgy@gmail.com>
2017-01-26 11:55:05 -0500

    Add rank to vote

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170126163735) do
+ActiveRecord::Schema.define(version: 20170126165427) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,6 +34,7 @@ ActiveRecord::Schema.define(version: 20170126163735) do
     t.datetime "updated_at", null: false
     t.integer  "album_id"
     t.integer  "ballot_id"
+    t.integer  "rank"
     t.index ["ballot_id"], name: "index_votes_on_ballot_id", using: :btree
   end
 
