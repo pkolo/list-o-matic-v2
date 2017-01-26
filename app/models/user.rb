16f84130db638a03de82902f9014d2b27230ba0a
Patrick Kolodgy <pkolodgy@gmail.com>
2017-01-26 16:22:01 -0500

    List and Ballot associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  has_many :lists
-  has_many :ballots
+  has_many :owned_lists, class_name: List, foreign_key: 'owner_id'
+  has_many :ballots, foreign_key: 'voter_id'
+  has_many :lists_voted_on, through: :ballots, source: :list
   has_many :votes, through: :ballots
 end
