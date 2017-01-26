abcf5760ad22aab6d298d4133956b5c4df5a44dc
Patrick Kolodgy <pkolodgy@gmail.com>
2017-01-26 11:22:21 -0500

    Add title and max to lists

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :ballots, only: [:new] do
+  resources :lists, only: [:new, :create, :show]
+
+  resources :ballots, only: [:new, :create, :show] do
     resources :votes, only: [:create]
   end
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170126003537) do
+ActiveRecord::Schema.define(version: 20170126161953) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,8 @@ ActiveRecord::Schema.define(version: 20170126003537) do
   create_table "lists", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "title"
+    t.integer  "maximum"
   end
 
   create_table "votes", force: :cascade do |t|
