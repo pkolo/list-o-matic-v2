126ff0afa849f655d77bc3fb6b90cc0cfb464be2
Patrick Kolodgy <pkolodgy@gmail.com>
2017-01-26 18:44:27 -0500

    Create action for list

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -7,8 +7,25 @@ class ListsController < ApplicationController
     @list = List.new
   end
 
+  def create
+    @list = List.new(list_params)
+    @list.owner = current_user
+
+    if @list.save
+      redirect_to @list
+    else
+      render 'new'
+    end
+  end
+
   def show
     @list = List.find(params[:id])
   end
+
+  protected
+
+    def list_params
+      params.require(:list).permit(:title, :maximum)
+    end
 
 end
