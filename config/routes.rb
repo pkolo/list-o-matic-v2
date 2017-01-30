0e8acd77ec2373fe950c6ed5299c0cea576d917d
Patrick Kolodgy <pkolodgy@gmail.com>
2017-01-30 17:40:30 -0500

    Create action for list closing

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,9 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'lists#index'
 
-  resources :lists, only: [:new, :create, :show, :index]
+  resources :lists, only: [:new, :create, :show, :index] do
+    get '/close', to: 'lists#close_list'
+  end
 
   resources :ballots, only: [:new, :create, :show] do
     resources :votes, only: [:create, :destroy]
