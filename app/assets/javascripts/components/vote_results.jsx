3c9b3289bfe49d1496eebec4e01ec9ff23385fe1
Patrick Kolodgy <pkolodgy@gmail.com>
2017-01-29 21:46:44 -0500

    Results indicator

--- app/assets/javascripts/components/vote_results.jsx
+++ app/assets/javascripts/components/vote_results.jsx
@@ -9,6 +9,7 @@ class VoteResults extends React.Component {
         {this.props.albums.map( (album) =>
           <VoteResult album={album} key={album.id} ballotID={this.props.ballotID} voteHelper={this.props.voteHelper} match={this.props.match}/>
         )}
+        (if a result appears in green, use that entry-- it's included on another list)
       </div>
     )
   }
