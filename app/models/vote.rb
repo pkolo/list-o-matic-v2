32798a93932b9252b5881b21af294278dfff0443
Patrick Kolodgy <pkolodgy@gmail.com>
2017-02-01 13:56:03 -0500

    Set up vte discog assoc

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -1,5 +1,6 @@
 class Vote < ApplicationRecord
   belongs_to :ballot
+  has_one :discog, :foreign_key => :album_id, :primary_key => :album_id
 
   validates :album_id, uniqueness: {scope: :ballot, message: "can only vote for album once per ballot"}
 end
