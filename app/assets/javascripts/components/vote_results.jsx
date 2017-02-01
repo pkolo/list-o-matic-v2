0e66f175e9b750b00424aed726449ff3261ad922
Patrick Kolodgy <pkolodgy@gmail.com>
2017-02-01 09:06:44 -0500

    Bad matches appear in red

--- app/assets/javascripts/components/vote_results.jsx
+++ app/assets/javascripts/components/vote_results.jsx
@@ -6,7 +6,7 @@ class VoteResults extends React.Component {
   render() {
     return(
       <div className="results-container">
-        (if a result appears in green, use that entry-- it's included on another list)
+        <p>(if a result appears in green, use that entry-- it's included on another list. if a result appears in red, you've already voted for that record)</p>
         {this.props.albums.map( (album) =>
           <VoteResult album={album} key={album.id} ballotID={this.props.ballotID} voteHelper={this.props.voteHelper} match={this.props.match} badMatch={this.props.badMatch}/>
         )}
