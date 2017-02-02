00850a69e08a703f06d34eb4f354cc0d90c1008d
Patrick Kolodgy <pkolodgy@gmail.com>
2017-02-02 17:02:32 -0500

    Discogs saving video and image url

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -6,6 +6,11 @@ class VotesController < ApplicationController
     album_data = get_album_data(params[:album_id])
     discog = Discog.find_or_create_by(album_id: params[:album_id], year: album_data["year"], title: album_data["title"], artist: album_data["artists"][0]["name"], label: album_data["labels"][0]["name"])
 
+    discog.img_url = album_data["images"][0]["uri"] if album_data["images"]
+    discog.video_url = album_data["videos"][0]["uri"] if album_data["videos"]
+
+    discog.save
+
     @ballot = Ballot.find(params[:ballot_id])
     @vote = Vote.new(ballot: @ballot, album_id: params[:album_id])
     @vote.rank = @ballot.votes.length + 1
@@ -23,11 +28,5 @@ class VotesController < ApplicationController
     vote.destroy
     render json: {message: "destroyed!"}
   end
-
-  protected
-
-    def vote_params
-      params.require(:ballot).permits(:album_id, :rank, :ballot_id)
-    end
 
 end
