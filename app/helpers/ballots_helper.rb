28aa1e6f486c8629397611666109e9c8758763ba
Patrick Kolodgy <pkolodgy@gmail.com>
2017-02-02 15:49:31 -0500

    Refactor API methods with auth

--- app/helpers/ballots_helper.rb
+++ app/helpers/ballots_helper.rb
@@ -8,26 +8,34 @@ module BallotsHelper
   end
 
   def album_search(params)
-    url = "https://api.discogs.com/database/"
+    url = "https://api.discogs.com"
 
     conn = Faraday.new(url: url) do |faraday|
       faraday.adapter Faraday.default_adapter
       faraday.response :json
     end
 
-    response = conn.get('search', release_title: params[:album], artist: params[:artist], year: params[:year], type: 'release', token: ENV["DISCOG_TOKEN"], per_page: 10, "User-Agent" => "List-O-Matic v2, +https://list-o-matic.herokuapp.com")
+    response = conn.get do |req|
+      req.headers['User-Agent'] = "List-O-Matic v2, +https://list-o-matic.herokuapp.com"
+      req.headers['Authorization'] = "Discogs key=#{ENV['DISCOG_KEY']}, secret=#{ENV['DISCOG_SECRET']}"
+      req.url '/database/search', release_title: params[:album], artist: params[:artist], year: params[:year], type: 'release', per_page: 10
+    end
     response.body
   end
 
   def get_album_data(album_id)
-    url = "https://api.discogs.com/"
+    url = "https://api.discogs.com"
 
     conn = Faraday.new(url: url) do |faraday|
       faraday.adapter Faraday.default_adapter
       faraday.response :json
     end
 
-    response = conn.get("releases/#{album_id}", "User-Agent" => "List-O-Matic v2, +https://list-o-matic.herokuapp.com")
+    response = conn.get do |req|
+      req.headers["User-Agent"] = "List-O-Matic v2, +https://list-o-matic.herokuapp.com"
+      req.headers['Authorization'] = "Discogs key=#{ENV['DISCOG_KEY']}, secret=#{ENV['DISCOG_SECRET']}"
+      req.url "/releases/#{album_id}"
+    end
     response.body
   end
 
