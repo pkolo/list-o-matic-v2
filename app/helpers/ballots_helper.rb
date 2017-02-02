d144d8382f277fea6a6f1e9a089a18ceed7bf8b1
Patrick Kolodgy <pkolodgy@gmail.com>
2017-02-02 17:07:13 -0500

    Update copy helper to populate new columns

--- app/helpers/ballots_helper.rb
+++ app/helpers/ballots_helper.rb
@@ -4,6 +4,11 @@ module BallotsHelper
     ballot.votes.each do |vote|
       album_data = self.get_album_data(vote.album_id)
       discog = Discog.find_or_create_by(album_id: vote.album_id, year: album_data["year"], title: album_data["title"], artist: album_data["artists"][0]["name"], label: album_data["labels"][0]["name"])
+
+      discog.img_url = album_data["images"][0]["uri"] if album_data["images"]
+      discog.video_url = album_data["videos"][0]["uri"] if album_data["videos"]
+
+      discog.save
     end
   end
 
