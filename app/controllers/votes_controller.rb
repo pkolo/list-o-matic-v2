dcca3bcaa339689a86ad39941a70dca54789d2f1
Patrick Kolodgy <pkolodgy@gmail.com>
2017-02-02 16:24:32 -0500

    Vote creates discog first

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -3,14 +3,14 @@ class VotesController < ApplicationController
   include BallotsHelper
 
   def create
+    album_data = get_album_data(params[:album_id])
+    discog = Discog.find_or_create_by(album_id: params[:album_id], year: album_data["year"], title: album_data["title"], artist: album_data["artists"][0]["name"], label: album_data["labels"][0]["name"])
+
     @ballot = Ballot.find(params[:ballot_id])
     @vote = Vote.new(ballot: @ballot, album_id: params[:album_id])
     @vote.rank = @ballot.votes.length + 1
 
     if @vote.save
-      album_data = get_album_data(@vote.album_id)
-      discog = Discog.create(album_id: @vote.album_id, year: album_data["year"], title: album_data["title"], artist: album_data["artists"][0]["name"], label: album_data["labels"][0]["name"])
-
       render json: { rank: @vote.rank, id: @vote.id, album_data: discog }
     else
       render json: { errors: @vote.errors.full_messages }, status: 422
