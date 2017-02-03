f076163ad48a00fd4c6f9e6fb2289409e641d092
Patrick Kolodgy <pkolodgy@gmail.com>
2017-02-03 16:12:22 -0500

    Add reviews to list_data JSON

--- app/models/list.rb
+++ app/models/list.rb
@@ -19,20 +19,21 @@ class List < ApplicationRecord
     }
   end
 
-  def tally_results
-    self.votes.select {|vote| vote.ballot.votes.length >= self.minimum }.select {|vote| vote.rank <= self.maximum}.each_with_object(Hash.new(0)) do |vote, memo|
-      memo[vote.album_id] += ((self.maximum + 1) - vote.rank)
-    end
-  end
-
   def get_list_results
     album_points = self.tally_results
     album_points.inject([]) do |memo, (album_id, points)|
       memo << {
-        album_data: get_album_data(album_id),
+        album_data: Discog.find_by(album_id: album_id),
         points: points,
-        voters: self.get_album_voters(album_id)
+        voters: self.get_album_voters(album_id),
+        reviews: self.get_album_reviews(album_id)
       }
+    end
+  end
+
+  def tally_results
+    self.votes.select {|vote| vote.ballot.votes.length >= self.minimum }.select {|vote| vote.rank <= self.maximum}.each_with_object(Hash.new(0)) do |vote, memo|
+      memo[vote.album_id] += ((self.maximum + 1) - vote.rank)
     end
   end
 
@@ -50,6 +51,13 @@ class List < ApplicationRecord
         memo << {id: voter.id, username: voter.username, ballot_id: ballot.id}
       end
       memo
+    end
+  end
+
+  def get_album_reviews(id)
+    reviews = self.votes.select {|vote| vote.album_id == id && vote.review != "" }
+    reviews.inject([]) do |memo, vote|
+      memo << {review: vote.review, username: vote.ballot.voter.username, ballot_id: vote.ballot.id}
     end
   end
 
