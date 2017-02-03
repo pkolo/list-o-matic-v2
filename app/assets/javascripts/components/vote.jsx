3deee521f8d127dd688409393f3ab6cd32eef2be
Patrick Kolodgy <pkolodgy@gmail.com>
2017-02-03 11:45:30 -0500

    Split vote into rows

--- app/assets/javascripts/components/vote.jsx
+++ app/assets/javascripts/components/vote.jsx
@@ -42,25 +42,29 @@ class Vote extends React.Component {
   render() {
     let voteID = "vote_" + this.props.vote.id
     return (
-      <div className="vote row" data-id={this.props.vote.id} id={voteID} onMouseEnter={this.handleHover} onMouseLeave={this.handleHover} >
-        <div className="rank col-md-1">
-          {this.props.vote.rank}
+      <div className="vote" data-id={this.props.vote.id} id={voteID} onMouseEnter={this.handleHover} onMouseLeave={this.handleHover} >
+        <div className="row">
+          <div className="rank col-md-1">
+            {this.props.vote.rank}
+          </div>
+
+          <div className="col-md-10 vote-info">
+            <p className="title">{this.props.vote.album_data.artist} - {this.props.vote.album_data.title}</p>
+            <p className="other-album-info">{this.props.vote.album_data.year} | {this.props.vote.album_data.label}</p>
+          </div>
+
+          <div className="delete-btn">
+            {this.state.showDelete && <button onClick={this.handleClick}>x</button>}
+          </div>
+
+          <div className="review-btn">
+            <button onClick={this.handleReviewClick}>+review</button>
+          </div>
         </div>
 
-        <div className="col-md-10 vote-info">
-          <p className="title">{this.props.vote.album_data.artist} - {this.props.vote.album_data.title}</p>
-          <p className="other-album-info">{this.props.vote.album_data.year} | {this.props.vote.album_data.label}</p>
+        <div className="row">
+          {this.state.showReviewForm && <ReviewForm />}
         </div>
-
-        <div className="delete-btn">
-          {this.state.showDelete && <button onClick={this.handleClick}>x</button>}
-        </div>
-
-        <div className="review-btn">
-          <button onClick={this.handleReviewClick}>+review</button>
-        </div>
-
-        {this.state.showReviewForm && <ReviewForm />}
       </div>
     )
   }
