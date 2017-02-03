53128c059cc86a70ee40368f1f99dafb08c7dafd
Patrick Kolodgy <pkolodgy@gmail.com>
2017-02-03 12:06:48 -0500

    Style review form

--- app/assets/javascripts/components/review.jsx
+++ app/assets/javascripts/components/review.jsx
@@ -6,7 +6,12 @@ class Review extends React.Component {
   render() {
     return (
       <div className="review-container">
-        <ReviewForm />
+        <div className="col-md-1">
+
+        </div>
+        <div className="col-md-10 review">
+          <ReviewForm />
+        </div>
       </div>
     )
   }
