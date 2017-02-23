bf6025ea737f59950d7fab1c17d35a1a1cfc73f1
Patrick Kolodgy <pkolodgy@gmail.com>
2017-02-23 10:28:23 -0500

    Change latest review format

--- app/assets/javascripts/components/latest_review.jsx
+++ app/assets/javascripts/components/latest_review.jsx
@@ -7,8 +7,7 @@ class LatestReview extends React.Component {
     let review = this.props.review
     return (
       <div className="ListResult">
-        <p>"{review.review}" - <a href={"/ballots/"+review.ballot_id}>{review.username}</a></p>
-        <p>{review.username} on {review.artist} - {review.title}</p>
+        <p>"{review.review}" - <a href={"/ballots/"+review.ballot_id}>{review.username}</a> on {review.artist} - {review.title}</p>
       </div>
     )
   }
